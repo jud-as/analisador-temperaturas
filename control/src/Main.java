1f7e804b028a1dbcabaa609d5d9c88a3a3da0c0d
jud-as <guilhermeferreira082@gmail.com>
2024-09-17 18:50:33 -0300

    leitor-diretorios

--- control/src/Main.java
+++ control/src/Main.java
@@ -1,16 +1,38 @@
+import java.io.File;
 import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Lista de cidades: \n");
-        CidadeCSV controlador = new CidadeCSV();
-        ArrayList<Cidade> listaCidades =  controlador.ListarCidade();
+    //Caminho relativo dos arquivos CSV
+    public static String dir = "./uploads/";
 
-        for(Cidade cidade : listaCidades) {
-            System.out.println(cidade);
+
+    public static void main(String[] args) {
+
+        System.out.println("Lista de cidades: \n");
+        File[] listaArquivos = leitorDiretorio(dir);
+
+        for (File arquivo : listaArquivos) {
+
+            ArrayList<Cidade> listaCidades =  CidadeCSV.listarCidade(dir+arquivo.getName());
+            for (Cidade cidade : listaCidades) {
+                System.out.println(cidade);
+            }
+
         }
+
     }
+
+    //Método para ler todos os arquivos do diretório
+    public static File[] leitorDiretorio(String dir) {
+
+        File[] arquivos;
+        File diretorio = new File(dir);
+        arquivos = diretorio.listFiles();
+
+        return arquivos;
+    }
+
 }
