98d41a66e917fc4d7e60eba2e39d1349289de1d4
jud-as <guilhermeferreira082@gmail.com>
2024-09-21 22:28:50 -0300

    Projeto pronto para uso de Threads

--- application/src/Main.java
+++ application/src/Main.java
@@ -16,32 +16,13 @@ public class Main {
         //Obter todas as cidades do diretório agrupadas por arquivo
         Map<String, ArrayList<Cidade>> cidadesPorArquivo = ServiceCidade.getDirCidades(dir);
 
-
         // Calcula as temperaturas
         Map<String, Map<Integer, Map<Integer, Map<String, Double>>>> temperaturasPorCidade = CalculoCidades.calcularTemperaturas(cidadesPorArquivo);
 
         // Exporta para TXT
-        CalculoCidades.exportarParaTxt(temperaturasPorCidade, "resultado.txt");
+        ServiceCidade.exportarParaTxt(temperaturasPorCidade, "resultado.txt");
 
     }
 
-    // Função para exportar dados para um arquivo .txt
-    public static void exportarDadosParaTxt(Map<String, Map<Integer, Map<Integer, List<Cidade>>>> dados, String caminhoArquivo) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo))) {
-            for (Map.Entry<String, Map<Integer, Map<Integer, List<Cidade>>>> entry : dados.entrySet()) {
-                writer.write("Arquivo: " + entry.getKey() + "\n");
-                for (Map.Entry<Integer, Map<Integer, List<Cidade>>> anoEntry : entry.getValue().entrySet()) {
-                    writer.write("  Ano: " + anoEntry.getKey() + "\n");
-                    for (Map.Entry<Integer, List<Cidade>> mesEntry : anoEntry.getValue().entrySet()) {
-                        writer.write("    Mês: " + mesEntry.getKey() + "\n");
-                        for (Cidade cidade : mesEntry.getValue()) {
-                            writer.write("      " + cidade + "\n");
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
 }
--- com.analisador.temperatura.resource/src/CalculoCidades.java
+++ com.analisador.temperatura.resource/src/CalculoCidades.java
@@ -68,39 +68,6 @@ public class CalculoCidades {
         return temperaturasPorCidade;
     }
 
-    // Método para exportar o resultado para um arquivo TXT
-    public static void exportarParaTxt(Map<String, Map<Integer, Map<Integer, Map<String, Double>>>> temperaturasPorCidade, String caminhoArquivo) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo))) {
-            for (Map.Entry<String, Map<Integer, Map<Integer, Map<String, Double>>>> arquivoEntry : temperaturasPorCidade.entrySet()) {
-                String nomeArquivo = arquivoEntry.getKey();
-                writer.write("Arquivo: " + nomeArquivo);
-                writer.newLine();
 
-                Map<Integer, Map<Integer, Map<String, Double>>> temperaturasPorAno = arquivoEntry.getValue();
-                for (Map.Entry<Integer, Map<Integer, Map<String, Double>>> anoEntry : temperaturasPorAno.entrySet()) {
-                    int ano = anoEntry.getKey();
-                    writer.write("  Ano: " + ano);
-                    writer.newLine();
-
-                    Map<Integer, Map<String, Double>> temperaturasPorMes = anoEntry.getValue();
-                    for (Map.Entry<Integer, Map<String, Double>> mesEntry : temperaturasPorMes.entrySet()) {
-                        int mes = mesEntry.getKey();
-                        writer.write("    Mês: " + mes);
-                        writer.newLine();
-
-                        Map<String, Double> temperaturas = mesEntry.getValue();
-                        writer.write("      Média: " + temperaturas.get("media"));
-                        writer.newLine();
-                        writer.write("      Máxima: " + temperaturas.get("maxima"));
-                        writer.newLine();
-                        writer.write("      Mínima: " + temperaturas.get("minima"));
-                        writer.newLine();
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
 
